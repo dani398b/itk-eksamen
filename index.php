0bee8fa25e0a9fbcbe4be410a7390b99973052fd
dani398b <46445116+dani398b@users.noreply.github.com>
2019-01-15 17:26:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Amatør Maple Hjælp</title>
+	<link rel="stylesheet" type="text/css" href="tema.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
+</head>
+<body>
+	<div class="sideHoved">
+		<h1>Amatør Maple Hjælp</h1>
+	</div>
+	<div class="sideUnderhoved">
+		<p>Oversigt</p><p>Indsend</p><p>Login</p>
+	</div>
+</form>
+</body>
+</html>
